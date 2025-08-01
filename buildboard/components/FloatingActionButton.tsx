ec8f078e3bcea396e4e894a0bde97131e0ad4350
Loveday Annointed <loveday8877@gmail.com>
2025-08-01 14:08:14 +0000

    test:navbar

--- buildboard/components/FloatingActionButton.tsx
+++ buildboard/components/FloatingActionButton.tsx
@@ -8,7 +8,7 @@ export default function FloatingActionButton({ href = "/new" }: { href?: string 
   return (
     <Link href={href}>
       <Button
-        className="fixed bottom-6 right-6 rounded-full w-14 h-14 p-0 text-white bg-black hover:bg-gray-800"
+        className="fixed bottom-10 right-6 rounded-full w-14 h-14 p-0 text-white bg-black hover:bg-gray-800"
         size="icon"
       >
         <Plus className="w-6 h-6" />
